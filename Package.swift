67964f85aa51e7888233f4c98c5a61f5601e0a43
Gabe The Coder <gabethecoder@icloud.com>
2022-10-06 11:09:48 -0400

    wtf

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "SwiftAV", 
                       url: "https://components.geekyinteractive.com/SwiftAV.xcframework.zip", 
-                      checksum: "16d8a584a12662fb0f39e46ae0680bd5c411d0e7bc13436c6b53be2c5ad21569")
+                      checksum: "95a369d08d84823ceba863fea4cbf113311bb4307560725103d7576cba4eb59f")
     ]
 )
